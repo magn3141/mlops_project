791766fd167f0814359d32bef422f8040ca1a5a7
Anders Bensen <andersbensen@MacBook-Pro-tilhrende-Anders.local>
2022-01-07 10:36:40 +0100

    data fix

--- src/models/transformers_poc.py
+++ src/models/transformers_poc.py
@@ -5,7 +5,7 @@ from src.data import TextDataset
 tokenizer = AutoTokenizer.from_pretrained("flax-community/dansk-gpt-wiki", model_max_length=512)
 model = AutoModelForCausalLM.from_pretrained("flax-community/dansk-gpt-wiki")
 
-dataset = TextDataset(path="./data/", tokenizer=tokenizer, max_length=512)
+dataset = TextDataset(path="./data/raw/press_conferences/", tokenizer=tokenizer, max_length=512)
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
