f00199a460794347ef93c08526f8f36960b7f274
AndersBensen <anders_bensen@hotmail.com>
2022-01-07 16:44:31 +0100

    made make dataset file

--- src/data/conference_dataset.py
+++ src/data/conference_dataset.py
@@ -1,20 +1,12 @@
-import os
+import torch
 from torch.utils.data import Dataset
 
 class TextDataset(Dataset):  
-    def __init__(self, path, tokenizer=None, max_length=512):
-        self.texts = []
-        self.tokenizer = tokenizer
-        files = os.listdir(path)
-        for f in files: 
-            with open(path+f) as file:
-                text = file.read().replace('\n',' ')
-                tokenized_text = self.tokenizer.encode(text, return_tensors='pt')[0][:max_length]
-                self.texts.append(tokenized_text)
+    def __init__(self, path):
+        self.texts = torch.load(path)
      
     def __len__(self):
         return len(self.texts)
 
     def __getitem__(self, i):
-        text = self.texts[i]
-        return text
+        return self.texts[i]
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,19 +1,47 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
+import os
+import torch
+from transformers import AutoTokenizer
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
+@click.argument('tensor_size')
+@click.argument('tokenizer')
 @click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+def main(input_filepath, tensor_size, tokenizer, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+        cleaned data ready to be analyzed (saved in ../processed). 
+        The tokenizer is used to tokenize the input data, where each data
+        point is a maxium size of tensor_size. 
     """
+    tensor_size = int(tensor_size)
+    
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+
+    press_conference_texts = []
+    files = os.listdir(input_filepath)
+    for f in files: 
+        with open(input_filepath+f) as file:
+            # data = file.read().replace('\n',' ')
+            data = file.read()
+            tokenized_text = tokenizer.encode(data, return_tensors='pt')
+            start = 0 
+            for i in data:
+                data_point = tokenized_text[0][start:start+tensor_size]
+                if (len(data_point) != tensor_size): 
+                    break
+                start += tensor_size 
+                press_conference_texts.append(data_point)
+    data_tensor = torch.stack(press_conference_texts)
+    torch.save(data_tensor, output_filepath)
 
 
 if __name__ == '__main__':
@@ -26,5 +54,4 @@ if __name__ == '__main__':
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
-
     main()
