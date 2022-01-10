869f3d334686a40e36178f070770c570da9fde5b
AndersBensen <anders_bensen@hotmail.com>
2022-01-10 22:02:54 +0100

    added wandb

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -6,6 +6,7 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 import datetime
 import numpy as np 
+import wandb
 
 # Model_max_length
 # Batch size
@@ -15,10 +16,12 @@ import numpy as np
 
 @hydra.main(config_path="config", config_name="train")
 def main(cfg: DictConfig):
+    wandb.init(entity="mlops_grp40", project="danish_gpt2")
 
     working_dir = hydra.utils.get_original_cwd()
 
     model = AutoModelForCausalLM.from_pretrained(cfg.backbone)
+    wandb.watch(model, log_freq=100)
 
     dataset = TextDataset(path=working_dir + "/data/processed/" + cfg.train_data)
     dataloader = torch.utils.data.DataLoader(
@@ -26,23 +29,36 @@ def main(cfg: DictConfig):
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model.train().to(device)
+
     optimizer = torch.optim.AdamW(params=model.parameters(), lr=cfg.lr)
+    min_loss = 1
+
     for epoch in range(cfg.n_epochs):
         print("--- Epoch:", epoch, "---")
         running_loss = []
         for i, batch in enumerate(dataloader):
-            outputs = model(batch, labels=batch)
+
+            optimizer.zero_grad()
+
+            outputs = model(batch.to(device), labels=batch.to(device))
+
             loss = outputs[0]
             loss.backward()
+
             optimizer.step()
-            optimizer.zero_grad()
+
             running_loss.append(loss.item())
+
             if (i%20 == 0):        
                 avg_loss = np.mean(running_loss)
                 print(f"Training loss: {avg_loss}")
 
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    model.save_pretrained(working_dir + "/models", cfg.name + "_"+now)
+        epoch_loss = np.mean(running_loss)
+        wandb.log({"train_loss": epoch_loss})
+
+        if (epoch_loss < min_loss):
+            now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            model.save_pretrained(working_dir + "/models", cfg.name + "_"+now)
 
 
 if __name__ == "__main__":
