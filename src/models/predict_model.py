3e105a09a98a5b4edebe0128a5d7dd24b2695328
Magnus Falkenberg <magn21falken@gmail.com>
2022-01-12 10:07:59 +0100

    Closes #1. Finished hydra

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -7,6 +7,9 @@ from src.data.conference_dataset import TextDataset
 import hydra
 from omegaconf import DictConfig, OmegaConf
 import datetime
+import logging
+log = logging.getLogger(__name__)
+
 
 # Model_max_length
 # Batch size
@@ -19,14 +22,19 @@ def main(cfg):
 
     working_dir = hydra.utils.get_original_cwd()
 
+    log.info("Loading model....")
     tokenizer = AutoTokenizer.from_pretrained("flax-community/dansk-gpt-wiki")
-    model = AutoModelForCausalLM.from_pretrained(working_dir + cfg.model_path)
+    model = AutoModelForCausalLM.from_pretrained(
+        working_dir + cfg.model_relative_path)
+    log.info("Model loaded.")
 
     tokenized_string = tokenizer.encode(cfg.text, return_tensors='pt')
     generated_text_encoded = model.generate(
         tokenized_string, max_length=cfg.max_length)
-    generated_text2 = tokenizer.decode(generated_text_encoded[0])
-    print(generated_text2)
+    generated_text = tokenizer.decode(generated_text_encoded[0])
+    with open('output.txt', 'w') as f:
+        f.write(generated_text)
+        log.info(generated_text)
 
 
 if __name__ == "__main__":
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,40 +1,56 @@
 from typing import Dict
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig
 import torch
 from src.data.conference_dataset import TextDataset
 import hydra
 from omegaconf import DictConfig, OmegaConf
 import datetime
-import numpy as np 
+import numpy as np
 import wandb
+import logging
+
 
 # Model_max_length
 # Batch size
 # Learning rate
 # n_epochs
+log = logging.getLogger(__name__)
 
 
 @hydra.main(config_path="config", config_name="train")
 def main(cfg: DictConfig):
-    wandb.init(entity="mlops_grp40", project="danish_gpt2")
+    wandb.init(entity=cfg.wandb_entity, project=cfg.wandb_project)
 
     working_dir = hydra.utils.get_original_cwd()
 
-    model = AutoModelForCausalLM.from_pretrained(cfg.backbone)
+    if (cfg.continue_training):
+        model = AutoModelForCausalLM.from_pretrained(
+            working_dir + "/models/" + cfg.name)
+        config = AutoConfig.from_pretrained(
+            working_dir + "/models/" + cfg.name)
+
+        min_loss = config.to_dict()["train_run"]["min_loss"]
+        log.info(
+            f"Continue training on /models/{cfg.name} with loss {min_loss}")
+    else:
+        log.info("Training {cfg.backbone}")
+        model = AutoModelForCausalLM.from_pretrained(cfg.backbone)
+        min_loss = 10e10
     wandb.watch(model, log_freq=100)
 
-    dataset = TextDataset(path=working_dir + "/data/processed/" + cfg.train_data)
+    dataset = TextDataset(
+        path=working_dir + "/data/processed/" + cfg.train_data)
     dataloader = torch.utils.data.DataLoader(
         dataset, batch_size=cfg.batch_size, shuffle=True)
+    log.info("Data loaded")
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model.train().to(device)
 
     optimizer = torch.optim.AdamW(params=model.parameters(), lr=cfg.lr)
-    min_loss = 1
 
     for epoch in range(cfg.n_epochs):
-        print("--- Epoch:", epoch, "---")
+        log.info(f"--- Epoch: {epoch} ---")
         running_loss = []
         for i, batch in enumerate(dataloader):
 
@@ -49,17 +65,25 @@ def main(cfg: DictConfig):
 
             running_loss.append(loss.item())
 
-            if (i%20 == 0):        
+            if (i % 20 == 0):
                 avg_loss = np.mean(running_loss)
-                print(f"Training loss: {avg_loss}")
+                log.info(f"Training loss: {avg_loss}")
 
         epoch_loss = np.mean(running_loss)
         wandb.log({"train_loss": epoch_loss})
 
         if (epoch_loss < min_loss):
             min_loss = epoch_loss
-            now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            model.save_pretrained(working_dir + "/models", cfg.name + "_"+now)
+            # now = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+
+            log.info(f"Saving new model with better loss ({min_loss})")
+            # save both in hydra output folder & in models folder
+            model.config.update(
+                {"train_run": {"min_loss": 2000}})
+            # min_loss
+            model.save_pretrained(f"{working_dir}/models/{cfg.name}")
+            model.save_pretrained("./")
+     # now = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
 
 
 if __name__ == "__main__":
