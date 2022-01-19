c2bb4a2151b88f562f883259b1e68626b5f9ed66
AndersBensen <anders_bensen@hotmail.com>
2022-01-19 09:56:54 +0100

    added bash script to run gcloud

---
+++ run_gcloud_train.sh
@@ -0,0 +1,17 @@
+export PROJECT_ID=mlops-project-338109
+export BUCKET_NAME=${PROJECT_ID}-aiplatform
+export IMAGE_REPO_NAME=gpt2_train_container_gpu
+export IMAGE_TAG=gpu_all_data
+export IMAGE_URI=gcr.io/$PROJECT_ID/$IMAGE_REPO_NAME:$IMAGE_TAG
+export REGION=us-central1
+export JOB_NAME=custom_container_job_gpu_$(date +%Y%m%d_%H%M%S)
+
+gcloud ai-platform jobs submit training $JOB_NAME \
+--scale-tier BASIC_GPU \
+--region $REGION \
+--master-image-uri $IMAGE_URI \
+-- \
+gcloud_training=True \
+gcloud_dir=$BUCKET_NAME \
+n_epochs=2 \
+batch_size=1
